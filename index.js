73d922b15b01bd5063c3004a7242770587b6f12c
casimmons-byte <casimmons@anamosa.k12.ia.us>
2025-11-05 23:46:54 +0000

    Js

---
+++ index.js
@@ -0,0 +1,14 @@
+let firstName = 'caleb';
+
+let jobTitle = 'dish';
+
+let city = 'somwhere';
+
+
+
+let newJobPost = `Dear ${firstName},
+Thank you for deciding to join us! 
+Youll be working as our new ${jobTitle} here in ${city}`
+
+
+document.getElementById("result").innerHTML = newJobPost;
